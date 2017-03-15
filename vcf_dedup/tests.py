b53e767866356cf51cf1d85631e8c04a4ec28d6a
priesgo <pablo.ferreiro@genomicsengland.co.uk>
2017-03-15 14:40:39 +0000

    using an abstract class for the VCF transformer

--- vcf_dedup/tests.py
+++ vcf_dedup/tests.py
@@ -1,14 +1,16 @@
 import unittest
-from vcf_dedup.tools.vcf_reader import VcfTransformer
+from vcf_dedup.tools.vcf_transformer import VcfDedupper
+import logging
 
 class VcfDedupTests(unittest.TestCase):
 
     def setUp(self):
+        logging.basicConfig(level=logging.DEBUG)
         self.input_vcf = "../resources/LP2000906-DNA_F01.somatic.atomic.left.split.vcf.gz"
         self.output_vcf = "../resources/LP2000906-DNA_F01.somatic.transformed.atomic.left.split.vcf"
 
 
 
     def test1(self):
-        self.vcf_transformer = VcfTransformer(self.input_vcf, self.output_vcf)
-        self.vcf_transformer.iterate_vcf()
+        self.vcf_transformer = VcfDedupper(self.input_vcf, self.output_vcf)
+        self.vcf_transformer.process_vcf()
---
+++ vcf_dedup/tools/vcf_transformer.py
@@ -0,0 +1,124 @@
+import vcf
+import os.path
+import logging
+from abc import ABCMeta, abstractmethod
+
+
+class AbstractVcfTransformer(object):
+
+    __metaclass__ = ABCMeta
+
+    def __init__(self, input_vcf_file, output_vcf_file):
+        """
+        Loads VCF reader and writer.
+        :param input_vcf_file: the input file
+        :param output_vcf_file: the output file
+        """
+
+        # loads reader
+        logging.info("Initialising VCF transformer...")
+        logging.info("Input VCF file: %s" % input_vcf_file)
+        logging.info("Output VCF file: %s" % output_vcf_file)
+        if os.path.exists(input_vcf_file):
+            try:
+                self.input_vcf_file = input_vcf_file
+                self.reader = vcf.VCFReader(filename = self.input_vcf_file)
+            except Exception, e:
+                logging.error("Error opening input VCF file: " + str(e))
+                raise ValueError("Error opening input VCF file: " + str(e))
+        else:
+            logging.error("Input VCF file does not exist!")
+            raise ValueError("Input VCF file does not exist!")
+        # loads writer
+        try:
+            self.output_vcf_file = output_vcf_file
+            self.writer = vcf.VCFWriter(open(self.output_vcf_file, 'w'), self.reader)
+        except Exception, e:
+            logging.error("Error opening output VCF file: " + str(e))
+            raise ValueError("Error opening output VCF file: " + str(e))
+        logging.info("Initialised!")
+
+    def __del__(self):
+        """
+        Closes reader and writer.
+        :return:
+        """
+
+        logging.info("Closing the VCF reader and writer...")
+        self.reader.close()
+        self.writer.close()
+        logging.info("Resources closed!")
+
+    def process_vcf(self):
+        """
+        PRE: the VCF is sorted so equal variants must be adjacent
+        :return:
+        """
+
+        logging.info("Starts processing the VCF...")
+        # iterates all variants
+        for variant in self.reader:
+            self.transform_variant(variant)
+        logging.info("Finished processing the VCF!")
+
+
+    @abstractmethod
+    def transform_variant(self, variant):
+        pass
+
+    def equal_variants(self, variant1, variant2):
+        """
+
+        :param variant1:
+        :param variant2:
+        :return:  a boolean indicating if the variants are equal
+        """
+        #TODO: implement something
+        return (variant1.CHROM == variant2.CHROM and variant1.POS == variant2.POS and
+                variant1.REF == variant2.REF)
+        # TODO: use also ALT, beware that this is a list
+        ## and variant1.ALT == variant2.ALT)
+
+
+class VcfDedupper(AbstractVcfTransformer):
+
+    variants = []
+
+    def transform_variant(self, variant):
+        """
+        Variant transformation. Stores all variants with equal coordinates and writes a merged result.
+        :param variant: the current variant
+        :return: None
+        """
+
+        # TODO: do something to the list variants
+        # Reads previous variant
+        prev_variant = None
+        if len(self.variants) > 0:
+            prev_variant = self.variants[len(self.variants) - 1]
+        if prev_variant is not None and not self.equal_variants(prev_variant, variant):
+            # transforms variants
+            merged_variant = None
+            is_passed = False
+            # merges all variants in one, takes the first one not filtered or just the first one
+            for variant in self.variants:
+                if merged_variant is None:
+                    merged_variant = variant
+                    is_passed = len(merged_variant.FILTER) == 0
+                elif not is_passed:
+                    if len(variant.FILTER) == 0:
+                        merged_variant = variant
+            # writes transformed variant
+            self.writer.write_record(merged_variant)
+            # resets variants memory
+            self.variants = [variant]
+        else:
+            self.variants.append(variant)
+
+
+
+
+
+
+
+
