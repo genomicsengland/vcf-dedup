13cc884a044d3644f9d653001d1928db445ffc7e
priesgo <pablo.ferreiro@genomicsengland.co.uk>
2017-03-20 10:25:33 +0000

    increasing version number

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages
 
 setup(
     name='vcf-dedup',
-    version='0.1.1',
+    version='0.1.3',
     packages=find_packages(),
     scripts=['scripts/vcf_dedupper'],
     url='',
