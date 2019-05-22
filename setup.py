6e75274c21c000915ba690d5a239a6f25a4ddd1c
priesgo <priesgoferreiro@gmail.com>
2019-05-22 13:03:51 +0100

    update setup config to publish to pypi

--- setup.py
+++ setup.py
@@ -3,15 +3,16 @@ from setuptools import find_packages
 
 setup(
     name='vcf-dedup',
-    version='0.4.7',
+    version='0.4.8',
+    description='Removal of duplicated variants from a VCF',
     packages=find_packages(),
     scripts=['scripts/vcf_dedupper'],
-    url='',
-    license='',
-    author='priesgo',
-    author_email='pablo.ferreiro@genomicsengland.co.uk',
-    description='', requires=['PyVCF', 'argparse'],
-    install_requires=[
-        'PyVCF==0.6.8'
-    ]
+    url='https://github.com/genomicsengland/vcf-dedup',
+    download_url='https://github.com/genomicsengland/vcf-dedup/archive/v0.4.8.tar.gz',
+    license='Apache',
+    author='Pablo Riesgo Ferreiro',
+    author_email='pablo.riesgo-ferreiro@genomicsengland.co.uk',
+    requires=['PyVCF', 'argparse'],
+    install_requires=['PyVCF==0.6.8'],
+    keywords=['VCF']
 )
